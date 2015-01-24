c8e2baa1ef0897f645091527599f83e5861e8a06
SanderSwi <157578ss>
2015-01-24 18:54:23 +0100

    gamedata merge

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -15,7 +15,6 @@ public class GameData
 	//Dirt, Trees, Grass, Rocks, Sand, Water, Ice, Spikes, Tard, Lava
 	public static float[][] probabilities = new float[][]
 	{
-<<<<<<< HEAD
 		/*new float[] { 0.5f, 0.0f, 0.5f, 0f, 0f, 0f, 0f, 0f, 0f, 0f},
 		new float[] { 0.4f, 0.0f, 0.3f, 0.3f, 0.0f, 0f, 0f, 0f, 0f, 0f},
 		new float[] { 0.4f, 0.0f, 0.3f, 0.3f, 0.0f, 0f, 0f, 0f, 0f, 0f},
@@ -23,10 +22,7 @@ public class GameData
 		new float[] { 0.4f, 0.0f, 0.3f, 0.3f, 0.0f, 0f, 0f, 0f, 0f, 0f}*/
 
 		new float[] { 0.4f, 0.35f, 0.25f, 0f, 0f, 0f, 0f, 0f, 0f, 0f},
-=======
-//		new float[] { 0.4f, 0.35f, 0.25f, 0f, 0f, 0f, 0f, 0f, 0f, 0f},
-		new float[] { 0.5f, 0.0f, 0.0f, 0.5f, 0f, 0f, 0f, 0f, 0f, 0f},
->>>>>>> origin/master
+
 		new float[] { 0.3f, 0.3f, 0.15f, 0.15f, 0.1f, 0f, 0f, 0f, 0f, 0f},
 		new float[] { 0.2f, 0.2f, 0.1f, 0.15f, 0.15f, 0.1f, 0.1f, 0f, 0f,0f },
 		new float[] { 0.27f, 0.0f, 0f, 0.30f, 0.16f, 0.1f, 0.08f, 0.04f, 0.05f,0f },
@@ -35,18 +31,7 @@ public class GameData
 
 	public static Dictionary<HexTile.HexType, HexTile.HexLogic> hexesLogic = new Dictionary<HexTile.HexType, HexTile.HexLogic> ()
 	{
-<<<<<<< HEAD
-		{ HexTile.HexType.Dirt, new HexTile.HexLogic(0.9f, 0) },
-		{ HexTile.HexType.Grass, new HexTile.HexLogic(1, 0) },
-		{ HexTile.HexType.Ice, new HexTile.HexLogic(1, 0) },
-		{ HexTile.HexType.Lava, new HexTile.HexLogic(1, 0) },
-		{ HexTile.HexType.Rocks, new HexTile.HexLogic(1, 0) },
-		{ HexTile.HexType.Sand, new HexTile.HexLogic(1, 0) },
-		{ HexTile.HexType.Spikes, new HexTile.HexLogic(1, 0) },
-		{ HexTile.HexType.Tard, new HexTile.HexLogic(1, 0) },
-		{ HexTile.HexType.Trees, new HexTile.HexLogic(1, 0) },
-		{ HexTile.HexType.Water, new HexTile.HexLogic(1, 0) },
-=======
+
 		{ HexTile.HexType.Dirt, new HexTile.HexLogic(1, true) },
 		{ HexTile.HexType.Grass, new HexTile.HexLogic(1, true) },
 		{ HexTile.HexType.Ice, new HexTile.HexLogic(1, true) },
@@ -57,6 +42,5 @@ public class GameData
 		{ HexTile.HexType.Tard, new HexTile.HexLogic(1, true) },
 		{ HexTile.HexType.Trees, new HexTile.HexLogic(0.5f, true) },
 		{ HexTile.HexType.Water, new HexTile.HexLogic(1, false) },
->>>>>>> origin/master
 	};
 }
