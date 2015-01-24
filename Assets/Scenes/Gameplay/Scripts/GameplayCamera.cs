4e565a16bc93e14ae8602727215e13cbff36d56b
Szymon <Majewski>
2015-01-24 13:49:38 +0100

    podczepiona kamera pd gameplay

---
+++ Assets/Scenes/Gameplay/Scripts/GameplayCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameplayCamera : MonoBehaviour
+{
+    public Transform m_trackingTarget;
+
+    private Vector3 m_baseShift;
+    private Vector3 m_velocity;
+
+    void Awake()
+    {
+        m_baseShift = m_trackingTarget.position - transform.position;
+    }
+
+	void Update()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, m_trackingTarget.position - m_baseShift, ref m_velocity, 0.5f);
+	}
+}
