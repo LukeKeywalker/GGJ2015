940b3c9b46f77bd39659e03a9e39890e443f28e2
SanderSwi <157578ss>
2015-01-24 20:13:13 +0100

    Revert "Revert "Revert "input top/down leg fix"""
    This reverts commit 4492a5c125417763a1386f668e36d16251e21dfa.

--- Assets/Core/LizardInput.cs
+++ Assets/Core/LizardInput.cs
@@ -37,8 +37,8 @@ public class LizardInput : MonoBehaviour
 
             SetPawBinding(LimbId.ArmLeft, InputType.Pad1Left);
             SetPawBinding(LimbId.ArmRight, InputType.Pad1Right);
-			SetPawBinding(LimbId.LegLeft, InputType.Pad2Left);
-			SetPawBinding(LimbId.LegRight, InputType.Pad2Right);
+            SetPawBinding(LimbId.LegLeft, InputType.Pad1Right);
+            SetPawBinding(LimbId.LegRight, InputType.Pad1Left);
         }
     }
 
