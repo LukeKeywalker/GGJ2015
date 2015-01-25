1d91d8ed6adda030e228351028341606d9314b48
SanderSwi <157578ss>
2015-01-25 11:38:25 +0100

    gameplay balance changes
    disabled blocks
    changed values

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -9,7 +9,7 @@ public class GameData
 
 	public static readonly int[] areaHeights = new int[]
 	{
-		0, 5, 10, 15, 20, 25
+		0, 10, 20, 30, 40, 50
 	};
 
 	//Dirt, Trees, Grass, Rocks, Sand, Water, Ice, Spikes, Tard, Lava
@@ -22,22 +22,21 @@ public class GameData
 		new float[] { 0.4f, 0.0f, 0.3f, 0.3f, 0.0f, 0f, 0f, 0f, 0f, 0f}*/
 
 		new float[] { 0.4f, 0.35f, 0.25f, 0f, 0f, 0f, 0f, 0f, 0f, 0f},
-
-		new float[] { 0.3f, 0.3f, 0.15f, 0.15f, 0.1f, 0f, 0f, 0f, 0f, 0f},
-		new float[] { 0.2f, 0.2f, 0.1f, 0.15f, 0.15f, 0.1f, 0.1f, 0f, 0f,0f },
-		new float[] { 0.27f, 0.0f, 0f, 0.30f, 0.16f, 0.1f, 0.08f, 0.04f, 0.05f,0f },
-		new float[] { 0.24f, 0f, 0f, 0.27f, 0.1f, 0f, 0.2f, 0.06f, 0.08f, 0.05f }
+		new float[] { 0.35f, 0.3f, 0.15f, 0.14f, 0f, 0.06f, 0f, 0f, 0f, 0f},
+		new float[] { 0.27f, 0.22f, 0.12f, 0.17f, 0f, 0.12f, 0.1f, 0f, 0f,0f },
+		new float[] { 0.27f, 0.0f, 0f, 0.30f, 0f, 0.17f, 0.16f, 0f, 0f,0f },
+		new float[] { 0.25f, 0f, 0f, 0.3f, 0f, 0.2f, 0.25f, 0f, 0f, 0f }
 	};
 
 	public static readonly Dictionary<HexTile.HexType, HexTile.HexLogic> hexesLogic = new Dictionary<HexTile.HexType, HexTile.HexLogic> ()
 	{
 
-		{ HexTile.HexType.Dirt, new HexTile.HexLogic(0.91f, true) },
-		{ HexTile.HexType.Grass, new HexTile.HexLogic(0.96f, true) },
+		{ HexTile.HexType.Dirt, new HexTile.HexLogic(0.93f, true) },
+		{ HexTile.HexType.Grass, new HexTile.HexLogic(0.97f, true) },
 		{ HexTile.HexType.Ice, new HexTile.HexLogic(0.3f, true) },
 		{ HexTile.HexType.Lava, new HexTile.HexLogic(1, false) },
 		{ HexTile.HexType.Rocks, new HexTile.HexLogic(0.94f, true) },
-		{ HexTile.HexType.Sand, new HexTile.HexLogic(0.95f, true) },
+		{ HexTile.HexType.Sand, new HexTile.HexLogic(0.85f, true) },
 		{ HexTile.HexType.Spikes, new HexTile.HexLogic(1, false) },
 		{ HexTile.HexType.Tard, new HexTile.HexLogic(0, true) },
 		{ HexTile.HexType.Trees, new HexTile.HexLogic(1, true) },
